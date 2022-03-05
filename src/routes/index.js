6e1780f6d01d3b3b40e8abe1d85446bd642eec02
Your Nameerwilgomez14 <erwildiaz@gmail.com>
2022-03-05 18:12:47 -0400

    add files

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,15 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.render('index.html', { title: 'oly xit' });
 });
+router.get('/catalogo', (req, res) => {
+    res.render('catalogo.html', { tittle: 'Catálogo' });
+});
+router.get('/calendario', (req, res) => {
+    res.render('calendario.html', { tittle: 'Calendario' });
+});
+
+router.get('/contacto', (req, res) => {
+    res.render('contacto.html', { tittle: 'Contactanos' });
+});
 
 module.exports = router;

2c04a65b83f47a5c6c05fc228e6ef1a9b5351929
Your Nameerwilgomez14 <erwildiaz@gmail.com>
2022-03-06 01:48:55 -0400

    update files

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.render('index.html', { title: 'oly xit' });
 });
-router.get("/", (req, res) => {
+router.get("/contact", (req, res) => {
     res.render('contact.html', { title: 'contact' });
 });
 

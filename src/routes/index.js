521df962307e0384d5294bac7ac66c716ab39fb3
Your Nameerwilgomez14 <erwildiaz@gmail.com>
2022-03-06 20:09:04 -0400

    update files

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 router.get("/", (req, res) => {
     res.render('index.html', { title: 'HAMSA' });
 });
-router.get("/contact", (req, res) => {
-    res.render('contact.html', { title: 'contact' });
+router.get("/contacto", (req, res) => {
+    res.render('contacto.html', { title: 'contacto' });
 });
 
 

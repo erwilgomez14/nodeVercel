3222c8ccafa9661d0f7ad126534d6bd8a6f9650e
Your Nameerwilgomez14 <erwildiaz@gmail.com>
2022-03-07 00:16:58 -0400

    add files

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ app.set('views engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 //routes
 app.use(require('./routes/index'));
 

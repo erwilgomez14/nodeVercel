8a3a1c91dd04437d2b3735e4dee59dbca2bc484a
Your Nameerwilgomez14 <erwildiaz@gmail.com>
2022-03-06 22:00:28 -0400

    update files

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,12 @@ router.get("/", (req, res) => {
 router.get("/contacto", (req, res) => {
     res.render('contacto.html', { title: 'contacto' });
 });
+router.get("/calendario", (req, res) => {
+    res.render('calender.html', { title: 'Calendario' });
+});
+router.get("/catalago", (req, res) => {
+    res.render('magazine.html', { title: 'Catalogo' });
+});
 
 
 module.exports = router;
